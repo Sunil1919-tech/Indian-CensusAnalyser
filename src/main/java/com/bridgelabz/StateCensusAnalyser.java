afd2e4e389d4165c24225242d9d26916394c7045
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-09-10 15:25:14 +0530

    Added: Welcome to Indian Census Analyser

---
+++ src/main/java/com/bridgelabz/StateCensusAnalyser.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+/**
+ * purpose-to create Indian States Census Analyser and States Code
+ * using CSV file
+ * @author Sunil
+ * @version 16.0
+ * @since 10/09/2021
+ */
+public class StateCensusAnalyser {
+    /*
+    Main method displaying welcome message
+    */
+    public static void main(String[] args)  {
+        System.out.println("Welcome to Indian States census Analyser simulation");
+    }
+}
